558adf5aa42ccd5d75b1a6698d0fb9ac16820212
miatran1109 <miatran1109@gmail.com>
2020-04-28 00:18:39 +0700

    Remove cache, add again after add .gitignore

--- app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f090027;
-        public static final int end = 0x7f090048;
-        public static final int gone = 0x7f090055;
-        public static final int invisible = 0x7f09005e;
-        public static final int left = 0x7f090062;
-        public static final int packed = 0x7f090076;
-        public static final int parent = 0x7f090078;
-        public static final int percent = 0x7f09007b;
-        public static final int right = 0x7f090081;
-        public static final int spread = 0x7f0900a2;
-        public static final int spread_inside = 0x7f0900a3;
-        public static final int start = 0x7f0900a8;
-        public static final int top = 0x7f0900bf;
-        public static final int wrap = 0x7f0900ce;
+        public static final int bottom = 0x7f090026;
+        public static final int end = 0x7f090043;
+        public static final int gone = 0x7f090050;
+        public static final int invisible = 0x7f090059;
+        public static final int left = 0x7f09005d;
+        public static final int packed = 0x7f090070;
+        public static final int parent = 0x7f090072;
+        public static final int percent = 0x7f090075;
+        public static final int right = 0x7f09007b;
+        public static final int spread = 0x7f09009c;
+        public static final int spread_inside = 0x7f09009d;
+        public static final int start = 0x7f0900a2;
+        public static final int top = 0x7f0900b8;
+        public static final int wrap = 0x7f0900c7;
     }
     public static final class styleable {
         private styleable() {}
